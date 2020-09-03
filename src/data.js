1699bd0cbb171e2a09fe36e997a807e8a7eea3fa
Kushwaha <jyoti.kushwaha@sap.com>
2020-09-03 17:55:12 +0530

    data lib

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const accountData = fs.readFileSync('./src/json/accounts.json',{encoding: 'utf8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('./src/json/users.json',{encoding: 'utf8'});
+const users = JSON.parse(userData);
+
+const writeJSON = () => {
+    fs.writeFileSync(path.join(__dirname , 'json/accounts.json'), accountsJSON,'utf8');
+    res.render('transfer',{message: "Transfer Completed"});
+}
+
+module.exports = {
+    accounts,
+    users,
+    writeJSON
+}
