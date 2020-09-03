4de4c79c4964b185b8ea07b9b9c3333f5f602e09
Kushwaha <jyoti.kushwaha@sap.com>
2020-09-03 18:14:04 +0530

    router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const accountRoutes = require('./routes/account');
+const servicesRoutes = require('./routes/services');
 
 const {accounts,users, writeJSON } = require('./data');
 
@@ -15,43 +17,13 @@ app.get('/',(req, res)=>{
     res.render('index',{title: 'Account Summary', accounts: accounts});
 });
 
-app.get('/savings',(req, res)=>{
-    res.render('account',{account: accounts.savings});
-});
-
-app.get('/checking',(req, res)=>{
-    res.render('account',{account: accounts.checking});
-});
-
-app.get('/credit',(req, res)=>{
-    res.render('account',{account: accounts.credit});
-});
+app.use('/account', accountRoutes);
 
 app.get('/profile',(req, res)=>{
     res.render('profile',{user: users[0]});
 });
 
-app.get('/transfer',(req, res)=>{
-    res.render('transfer');
-});
-
-app.post('/transfer',(req,res)=>{
-    accounts[req.body.from].balance -= req.body.amount; 
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer',{message: "Transfer Completed"});
-});
-
-app.get('/payment',(req, res)=>{
-    res.render('payment', {account: accounts.credit});
-});
-
-app.post('/payment', (req, res)=>{
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available = parseInt(req.body.amount) + parseInt(accounts.credit.available);
-    writeJSON();
-    res.render('payment',{message: "Payment Successful", account: accounts.credit});
-});
+app.use('/services', servicesRoutes);
 
 app.listen(3000,()=>{
     console.log('PS Project Running on port 3000!');
